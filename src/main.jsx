34117ee2f7a1dfa403e72c86d20bf74d4ae4d986
Anurag chamoli <anuragchamoli2003@gmail.com>
2026-09-15 20:37:53 +0530

    fix router bug

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,12 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./routs/App.jsx";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createHashRouter, RouterProvider } from "react-router-dom";
 import Items from "./components/Items.jsx";
 import Cart1 from "./components/Carts/Carts-1.jsx";
 import VedaData from "./components/aboutDhrma/VedaData.jsx";
 
-const router = createBrowserRouter(
+const router = createHashRouter(
   [
   { path: "/Aacharya", element: <App />, children:[
     {index: true, element: < Items/>},
